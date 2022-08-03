1497b7e7af2e5fa49cd6bbf84f749f9b568332ac
Louise <62265200+L0ll094@users.noreply.github.com>
2022-08-03 12:07:06 +0200

    Setup a layout and working on string parsing

--- src/app/view-log/view-log.component.ts
+++ src/app/view-log/view-log.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { stringToKeyValue } from '@angular/flex-layout/extended/style/style-transforms';
 import {FormControl, Validators} from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
@@ -17,9 +18,11 @@ interface Container{
 
 
 export class ViewLogComponent implements OnInit {
+  selectedContainer: string= "";
+  containerChosen:Boolean = false;
   oneContainer_Control = new FormControl<Container | null>(null, Validators.required);
   selectFormControl = new FormControl('', Validators.required);
-  animals: Container[] = [
+  containers: Container[] = [
     {name: 'First', ID: 'd4flk2lnlk1'},
     {name: 'Second', ID: 'd4flk2lnlk2!'},
     {name: 'Third', ID: 'd4flk2lnlk3'},
@@ -30,4 +33,46 @@ export class ViewLogComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  clickedButton(): void {
+    this.containerChosen = !this.containerChosen;
+  }
+  readAndParseFile(): string {
+    var allLinesInFile: String[][];
+
+
+    fetch('assets/dockerinformation.txt')
+    .then(response => response.text())
+    .then(text=> {
+      allLinesInFile = text.split('\n').map((line) => line.split('\t'));
+      console.log("Before:");
+      console.log(allLinesInFile);
+      for (let i = 0; i<allLinesInFile.length-1; i++){
+        let temp1=allLinesInFile[i][0].split('   ');
+        console.log("After:");
+        console.log(allLinesInFile);
+       
+        //Access the name of the container
+        console.log("Name:");
+        console.log(temp1[temp1.length-1]);
+        
+        //Accessing the status of the container
+        //console.log(allLinesInFile[i][0].split('ago   '))
+        let othertemp=allLinesInFile[i][0].split('ago   ');
+        let statusline=othertemp[1];
+        console.log(statusline);
+        let status="hi";//statusline.split('0.0');
+
+        console.log("Status:");
+        console.log(status);
+      }
+      //console.log("LinesSeparatedByThreeSpaces");
+      //onsole.log(linesSeparatedByThreeSpaces);
+      //let firstLine=allLinesInFile[1][0].split('ago   ');
+      //let status=firstLine[1].split('0.0')[0];
+ 
+    } )
+
+    //parsedString = tabbedString.split('\n').map((line) => line.split('\t'))
+    return "hello";
+  }
 }
